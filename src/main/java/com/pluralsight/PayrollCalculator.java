5f41ebe2fc9ba1ed17d72b9a4941f3d28418232a
sarakibalogunworks <sarakibalogunworks@gmail.com>
2026-04-20 17:49:29 -0400

    added try code commit

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,4 +1,20 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class PayrollCalculator {
+
+    public static void main(String[] args) {
+
+        String fileName = "employee.csv";
+
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+
+        }
+    }
 }
