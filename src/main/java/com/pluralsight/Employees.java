557989fcef1dede5949ffcdbab9bebb3adc8f370
sarakibalogunworks <sarakibalogunworks@gmail.com>
2026-04-20 17:40:46 -0400

    added line 17-50 commit

--- src/main/java/com/pluralsight/Employees.java
+++ src/main/java/com/pluralsight/Employees.java
@@ -1,22 +1,52 @@
 package com.pluralsight;
 
-public class Employees {
+public class Employee {
     private int employeeId;
     private String name;
     private double hoursWorked;
     private double payRate;
 
 
-    public Employees(String name, int employeeId, double payRate, double hoursWorked) {
+    public Employee(String name, int employeeId, double payRate, double hoursWorked) {
         this.name = name;
         this.employeeId = employeeId;
         this.payRate = payRate;
         this.hoursWorked = hoursWorked;
     }
 
-    public Employee(int employeeId, String name, double hoursWorked, double payRate) {
-
+    public double getGrossPay() {
+        return this.hoursWorked * this.payRate;
+    }
+    public int getEmployeeId() {
+        return this.employeeId;
     }
 
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
 
 }
